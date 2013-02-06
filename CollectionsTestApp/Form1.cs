b553158aad0fc8d2443866cedf7a1dedb07414ec
izrik <izrik@yahoo.com>
2013-02-05 23:08:02 -0600

    Updated solution file to VS 2010. Added the CollectionTestApp project files.

---
+++ CollectionsTestApp/Form1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MetaphysicsIndustries.Collections;
+
+namespace Collections_TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set<int> numbers = new Set<int>();
+            //Set<char> letters = new Set<char>();
+
+            //numbers.Add(1);
+            //numbers.Add(4);
+            //numbers.Add(5);
+
+            //letters.Add('a');
+            //letters.Add('M');
+            //letters.Add('q');
+            //letters.Add('a');
+
+            //this.Close();
+
+        }
+    }
+}
